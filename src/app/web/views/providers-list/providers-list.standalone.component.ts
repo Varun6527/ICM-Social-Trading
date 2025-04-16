69ca7c5bec818e9bfd6a9811860082b3bbf88574
Varun6527 <varun.srivastava@mail.vinove.com>
2025-04-16 14:13:20 +0530

    chart issue resolved

--- src/app/web/views/providers-list/providers-list.standalone.component.ts
+++ src/app/web/views/providers-list/providers-list.standalone.component.ts
@@ -223,18 +223,18 @@ export class ProvidersListStanAloneComponent {
       enablePagination: true,
       headerNameLangArr: colDefs.map((o: any) => o.headerName),
       rowModelType: 'clientSide',
-      rowHeight: 70
+      rowHeight: 110
     }
   }
 
   setupRatingConfig() {
     let colDefs = [
-      { field: "accountName", headerName: 'PROVIDERS_LIST.Title', resizable: false, width: 250, suppressSizeToFit: true, cellRenderer: StrategyCellRendererStandAloneComponent, cellRendererParams: { ratingId: this.ratingId  } },
-      { field: "extension.investors", headerName: 'PROVIDERS_LIST.Investors', resizable: false, width: 150, valueFormatter: (params: any) => params.value ? params.value : '-' },
-      { field: "extension.investorsEquityUsd", headerName: 'PROVIDERS_LIST.Invested', resizable: false, width: 150, cellRenderer: CommonCellRendererStandAloneComponent, colId: 'currencyCell' },
-      { field: "account.balance", headerName: 'PROVIDERS_LIST.Own Funds', resizable: false, width: 150, cellRenderer: CommonCellRendererStandAloneComponent, colId: 'currencyCell' },
-      { field: "maxDrawdown", headerName: 'PROVIDERS_LIST.Drawdown', resizable: false, width: 150, valueFormatter: (params: any) => params.value || params.value == 0 ? params.value + '%' : '-' },
-      { field: "extension.performanceFee", headerName: 'PROVIDERS_LIST.Fee', resizable: false, width: 150, valueFormatter: (params: any) => params.value || params.value == 0 ? params.value + '%' : '-' },
+      { field: "accountName", cellClass: 'd-flex align-center', headerName: 'PROVIDERS_LIST.Title', resizable: false, width: 250, suppressSizeToFit: true, cellRenderer: StrategyCellRendererStandAloneComponent, cellRendererParams: { ratingId: this.ratingId  } },
+      { field: "extension.investors",  cellClass: 'd-flex align-center', headerName: 'PROVIDERS_LIST.Investors', resizable: false, width: 150, valueFormatter: (params: any) => params.value ? params.value : '-' },
+      { field: "extension.investorsEquityUsd", cellClass: 'd-flex align-center', headerName: 'PROVIDERS_LIST.Invested', resizable: false, width: 150, cellRenderer: CommonCellRendererStandAloneComponent, colId: 'currencyCell' },
+      { field: "account.balance", cellClass: 'd-flex align-center', headerName: 'PROVIDERS_LIST.Own Funds', resizable: false, width: 150, cellRenderer: CommonCellRendererStandAloneComponent, colId: 'currencyCell' },
+      { field: "maxDrawdown", cellClass: 'd-flex align-center', headerName: 'PROVIDERS_LIST.Drawdown', resizable: false, width: 150, valueFormatter: (params: any) => params.value || params.value == 0 ? params.value + '%' : '-' },
+      { field: "extension.performanceFee", cellClass: 'd-flex align-center', headerName: 'PROVIDERS_LIST.Fee', resizable: false, width: 150, valueFormatter: (params: any) => params.value || params.value == 0 ? params.value + '%' : '-' },
       { field: "historyGraph.value", headerName: 'PROVIDERS_LIST.Chart', resizable: false, cellRenderer: ChartCellRendererStandAloneComponent, width: 200},
       { field: "action", sortable: false, headerName: '', resizable: false, cellRenderer: ActionButtonStanAloneComponent, colId: 'copyTradeBtnCell', width: 150, cellStyle: { 'padding-left': '40px', 'display': 'flex', 'justify-content': 'center', 'align-items': 'center' } },
     ];
