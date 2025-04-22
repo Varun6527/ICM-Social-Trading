064f938606757e82ff3db812f3608f640f9f261d
Varun6527 <varun.srivastava@mail.vinove.com>
2025-04-22 12:55:44 +0530

    side nav trasnation added and new header added for mobile view

--- src/app/web/app/web-layout.component.ts
+++ src/app/web/app/web-layout.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { WebService } from '../service/web.service';
 
 @Component({
   selector: 'app-web-layout',
   template: `
     <div class="web-layout">
-      <div class="sidenav">
+      <div class="sidenav" [ngClass]="{'active': showSideNav}">
           <app-sidenav></app-sidenav>
+          <div class="sidenave-close-wrapper" (click)="toggleSideNavBar()"></div>
       </div>
+    <mat-card appearance="outlined" class="section-card header-logo">
+      <mat-card-content class="section-card-content">
+        <img width="100" src="../../../assets/logo/ICM Logo Default 1.png" />
+        <img width="20" src="../../../assets/logo/Hamburger-icon.png" (click)="toggleSideNavBar()" />
+      </mat-card-content>
+    </mat-card>
       <div class="pages">
           <router-outlet></router-outlet>
       </div>
@@ -15,7 +23,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./web-layout-comonent.scss']
 })
 export class WebLayoutComponent {
+  showSideNav: boolean = false;
   
-  constructor() {
+  constructor(private _webService: WebService) {
+    this._webService.subscribeOnWebDataChange('WebLayoutComponent', (event: any)=>{
+      if(event.action == "close_sidenav_menu_options") {
+        this.toggleSideNavBar();
+      }
+    });
+  }
+
+  toggleSideNavBar() {
+    this.showSideNav = !this.showSideNav;
+  }
+
+  ngOnDestroy() {
+    this._webService.unSubscribeOnWebDataChange('WebLayoutComponent');
   }
 }
